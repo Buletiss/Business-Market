c97cf23fa8f065578e3f340a8dc6e44627bc6fa7
Vinicius Souza Feitosa <vinicius1997feitosa@gmail.com>
2021-02-28 18:19:52 -0300

    feature: mobile Page3

--- src/pages/Page3/styles.js
+++ src/pages/Page3/styles.js
@@ -7,12 +7,19 @@ export const Container = styled.div`
   height: 84vh;
   display: flex;
   justify-content: space-between;
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 export const LeftSide = styled.div`
   display: flex;
   width: 50%;
   background: linear-gradient(59.46deg, #1d737e -67.96%, #4fb9c7 103.75%);
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 export const RectangleWrapper = styled.div`
@@ -32,15 +39,17 @@ export const RectangleRight = styled.img.attrs({
 `;
 
 export const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   color: white;
-  position: absolute;
-  width: 463px;
-  height: 285px;
-  left: 384px;
-  top: 2724px;
-
   font-size: 15px;
   line-height: 180%;
+
+  @media screen and (max-width: 768px) {
+    line-height: 180%;
+    margin-left: -100px;
+  }
 `;
 
 export const Title = styled.div`
@@ -50,13 +59,22 @@ export const Title = styled.div`
   text-transform: uppercase;
   line-height: 300%;
   margin-top: -150px;
+
+  @media screen and (max-width: 768px) {
+    margin-top: 0px;
+    line-height: 250%;
+  }
 `;
 
 export const Text = styled.div`
   font-family: 'Fira Sans';
   margin-top: 100px;
   width: 463px;
-  height: 283px;
+
+  @media screen and (max-width: 768px) {
+    width: 463px;
+    margin-top: 30px;
+  }
 `;
 
 export const PrimaryText = styled.strong`
@@ -72,6 +90,9 @@ export const RightSide = styled.div`
   flex-direction: column;
   width: 50%;
   background: black;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 export const TitleRight = styled.div`
