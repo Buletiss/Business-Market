4b8347abe4802e4fbedf54cd5c2c2138d0aa6f0d
Vinicius Souza Feitosa <vinicius1997feitosa@gmail.com>
2021-02-28 17:11:52 -0300

    feature: mobile banner1

--- src/components/Banner1/styles.js
+++ src/components/Banner1/styles.js
@@ -12,21 +12,37 @@ export const Container = styled.div`
   align-items: center;
   justify-content: space-between;
   padding: 0 10rem;
+
+  @media screen and (max-width: 768px) {
+    background-position: center left 20%;
+  }
 `;
 
 export const LogoAndTitle = styled.div`
-  width: 40%;
+  width: 35%;
   display: flex;
   align-items: center;
   justify-content: space-between;
   color: white;
   font-family: 'Oswald';
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    justify-content: center;
+    font-size: 10px;
+    height: 100px;
+    width: 200px;
+    margin-left: -100px;
+  }
 `;
 
 export const Logo = styled.img.attrs({
   src: Logo2,
 })`
   margin-right: 3rem;
+  @media screen and (max-width: 768px) {
+    width: 50%;
+    margin-bottom: 20px;
+  }
 `;
 
 export const TextWrapper = styled.div`
@@ -38,6 +54,13 @@ export const TextWrapper = styled.div`
   line-height: 180%;
   max-width: 600px;
   color: white;
+
+  @media screen and (max-width: 768px) {
+    font-size: 13px;
+    position: relative;
+    left: 80px;
+    width: 75%;
+  }
 `;
 
 export const PrimaryText = styled.strong`
